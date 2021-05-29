551a5e1a0d36d98ab6407d45acc5f6f7fe18f323
Jonathan La Mela <jonathan.la.mela@gmail.com>
2021-05-29 10:29:46 +0200

    fixing import

--- lib/screens/Home/components/UltimeUscite.dart
+++ lib/screens/Home/components/UltimeUscite.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:radioflash/models/NewSongRelease.dart';
