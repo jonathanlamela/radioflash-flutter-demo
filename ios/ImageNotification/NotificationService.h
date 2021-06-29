62bb308478e57469f77c3d54217ca46090ee1cf5
jonathanlamela <jonathan.la.mela@gmail.com>
2021-06-29 19:30:21 +0200

    ios publish

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Jonathan La Mela on 29/06/21.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
